853adfddcd750fe790dd197cc8e9929da3e6a6cf
Joe Janicki <j.janicki@oteraaudio.com>
2026-07-01 14:14:52 +0200

    Initial basic home page

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import jinja2
 
 TEMPLATES_TO_RENDER = [
     "index.html",
+    "contribute.html",
     "style.css",
+    "wjosglogo.svg",
 ]
 
 TEMPLATE_DIR = 'templates'
 RENDER_DIR = 'docs'
+
+class HyperLink:
+    def __init__(self, text, link):
+        self.text = text
+        self.url = link
 
 '''
 Basic script to render out templates into their respective files.
@@ -16,7 +23,11 @@ def main():
     for template_name in TEMPLATES_TO_RENDER:
         template = env.get_template(template_name)
         with open(f"{RENDER_DIR}/{template_name}", "w") as fp:
-            fp.write(template.render())
+            fp.write(template.render(hyperlinks=[
+                HyperLink("Home", "/"),
+                HyperLink("Projects", "/"),
+                HyperLink("Contribute", "/contribute.html"),
+            ]))
 
     return
 
